766f5cbadb2fe486639f2267e71843a92844a9c4
Gegas96 <gegasanugrah@gmail.com>
2023-11-02 10:24:37 +0700

    jobsheet10

--- Numbers11.java
+++ Numbers11.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Numbers11 {
     public static void main(String[] args) {
         
@@ -6,6 +8,10 @@ public class Numbers11 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));            
+        }
 
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println("Panjang baris ke-"+ (i+1)+" : "+ myNumbers[i].length);    
